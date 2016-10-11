1e0c55df7949e10481f109ad7730726ae637e1bd
root <root@mekcity.com>
2016-10-11 01:30:39 -0400

    Now displays instructions and domain fixes

--- muxstats-sql.pl
+++ muxstats-sql.pl
@@ -8,6 +8,8 @@ $WEBDIR="/muxstats/";
 $LOGO="logoSmall.png";
 $SERVER_NAME="MekCity";
 $REVVER="1.0";
+$USERNAME_FIELD=16;
+$SHOWHOST_FIELD=17;
 
 my $UserID;
 my $RealName;
@@ -26,7 +28,6 @@ sub ConvertName ()
 # Load our dependancies
 #use strict;
 use File::Copy qw(copy);
-#use Net::Telnet();
 use DBI;
 
 my ($t, @output);
@@ -86,8 +87,8 @@ body {
 <img src="$WEBDIR$LOGO"><br>
 Last Scanned: $LAST_SEEN
 <table border=1>
-<tr><td colspan = 5><center><h1>$SERVER_NAME Server Stats</h1></center></td></tr>
-<tr><td><b>User Name</b></td><td><b>User Alias</b></td><td><b>On For</b></td><td><b>Idle</b></td><td><b>Last Logon</b></td></tr>
+<tr><td colspan = 6><center><h1>$SERVER_NAME Server Stats</h1></center></td></tr>
+<tr><td><b>User Name</b></td><td><b>User Alias</b></td><td><b>On For</b></td><td><b>Idle</b></td><td><b>Host</b></td><td><b>Last Logon</b></td></tr>
 END_MESSAGE
 open(my $fh, '>', "index.html") or die "Could not open file 'index.html' $!";
 print $fh $message;
@@ -109,9 +110,10 @@ $convertedname = "";
 my $dbh2 = DBI->connect('DBI:mysql:joomla', 'btmux-read', 'abcd1234')
    or die "Couldn't open database: $DBI::errstr; stopped";
 
+# ---
 # Prepare the SQL query for execution
 my $sth1 = $dbh2->prepare(<<End_SQL) or die "Couldn't prepare statement: $DBI::errstr; stopped";
-SELECT user_id, value FROM jml_community_fields_values WHERE value = "$PlayerName"
+SELECT user_id, field_id, value FROM jml_community_fields_values WHERE value = "$PlayerName"
 End_SQL
 
 # Execute the query
@@ -133,10 +135,26 @@ $sth2->execute() or die "Couldn't execute statement: $DBI::errstr; stopped";
 while ( my ($user_id, $user_realname, $username) = $sth2->fetchrow_array() ) {
     $RealName = $user_realname;
     print STDOUT "User Field 1: $RealName\n";
-
-
 }
 }
+# ---
+$ShowHost = "No";
+my $sth3 = $dbh2->prepare(<<End_SQL3) or die "Couldn't prepare statement: $DBI::errstr; stopped";
+SELECT user_id, field_id, value FROM jml_community_fields_values WHERE user_id = "$UserID"
+End_SQL3
+# Execute the User query
+$sth3->execute() or die "Couldn't execute statement: $DBI::errstr; stopped";
+
+# Fetch each row and print it
+while ( my ($user_id, $field_id, $value) = $sth3->fetchrow_array() ) {
+    if ($field_id == $SHOWHOST_FIELD)
+    {
+	$ShowHost = $value;
+    }
+    print STDOUT "Show Host = $ShowHost\n";
+}
+
+# ---
 
 # Convert name to url format
 $convertedname = ConvertName();
@@ -147,13 +165,17 @@ print ("URL: '$URL_IS'\n");
 # Disconnect from the database
 $dbh2->disconnect();
 
+	if ($ShowHost eq "No")
+	{
+	    $player_host = "-SECRET-";
+	}
 	if ($URL_IS ne "https://MekCity.com/index.php/community/-/profile")
 	{
-		print $fh "<tr><td width=250><a href='$URL_IS'>$PlayerName</a></td><td>$player_alias</td><td>$player_online</td><td>$player_idle</td><td>$player_last_logon</td></tr>";
+		print $fh "<tr><td width=250><a href='$URL_IS'>$PlayerName</a></td><td>$player_alias</td><td>$player_online</td><td>$player_idle</td><td>$player_host</td><td>$player_last_logon</td></tr>";
 	}
 	else
 	{
-		print $fh "<tr><td width=250>$PlayerName</td><td>$player_alias</td><td>$player_online</td><td>$player_idle</td><td>$player_last_logon</td></tr>";
+		print $fh "<tr><td width=250>$PlayerName</td><td>$player_alias</td><td>$player_online</td><td>$player_idle</td><td>$player_host</td><td>$player_last_logon</td></tr>";
 	}
 }
 
@@ -161,7 +183,18 @@ print $fh "</table>
 <hr>
 Max Users: $nrecord
 <hr>
-Version $REVVER - <b>Get This Utility At <a href=\"https://MekCity.org\">Mek City</a></b>
+Version $REVVER - <b>Get This Utility At <a href=\"https://MekCity.com\">Mek City</a></b><br>
+Want Your Profile Linked? Look at the instructions <a href=\"https://MekCity.com/profile_link.html\" target=_blank>here</a>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-3060462-34', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>";
 close $fh;
